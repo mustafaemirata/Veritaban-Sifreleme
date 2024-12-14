4e6589f90351485201b5b9da450f30804013fdbb
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-12-14 05:23:19 +0300

    Add files via upload

---
+++ SifreliVeriler/SifreliVeriler/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SifreliVeriler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtHesapNoo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1176, 51);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1122, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 327);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1176, 397);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtHesapNoo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtMail);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(167, 86);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(812, 188);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgiler";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(532, 138);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtHesapNoo
+            // 
+            this.txtHesapNoo.Location = new System.Drawing.Point(535, 86);
+            this.txtHesapNoo.Name = "txtHesapNoo";
+            this.txtHesapNoo.Size = new System.Drawing.Size(146, 34);
+            this.txtHesapNoo.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 28);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hesap No:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(535, 42);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(146, 34);
+            this.txtSifre.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(390, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Şifre:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(159, 135);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(146, 34);
+            this.txtMail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mail:";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(159, 86);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(146, 34);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyad:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(159, 42);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(146, 34);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 722);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtHesapNoo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SifreliVeriler/SifreliVeriler/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SifreliVeriler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void listele()
+        {
+            // SQL'den tüm verileri çeker
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Tbl_Veriler", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            // Yeni tablo oluştur (çözülmüş veriler için)
+            DataTable cozumluDt = new DataTable();
+
+            // Orijinal sütunları tabloya ekler
+            cozumluDt.Columns.Add("AD", typeof(string));
+            cozumluDt.Columns.Add("SOYAD", typeof(string));
+            cozumluDt.Columns.Add("MAIL", typeof(string));
+            cozumluDt.Columns.Add("SIFRE", typeof(string));
+            cozumluDt.Columns.Add("HESAPNO", typeof(string));
+
+            // Her bir satırı işler ve şifre çözme işlemini yapar
+            foreach (DataRow row in dt.Rows)
+            {
+                DataRow newRow = cozumluDt.NewRow();
+
+                // Şifrelenmiş veriyi çözüyor
+                newRow["AD"] = SifreCoz(row["AD"].ToString());
+                newRow["SOYAD"] = SifreCoz(row["SOYAD"].ToString());
+                newRow["MAIL"] = SifreCoz(row["MAIL"].ToString());
+                newRow["SIFRE"] = SifreCoz(row["SIFRE"].ToString());
+                newRow["HESAPNO"] = SifreCoz(row["HESAPNO"].ToString());
+
+                // Yeni tabloya ekler
+                cozumluDt.Rows.Add(newRow);
+            }
+
+            // Çözülmüş tabloyu DataGridView'e bağlar
+            dataGridView1.DataSource = cozumluDt;
+        }
+
+        // Şifre çözme işlemi
+        string SifreCoz(string sifreliVeri)
+        {
+            try
+            {
+                byte[] cozumDizi = Convert.FromBase64String(sifreliVeri); // Base64 çözümleme
+                return ASCIIEncoding.ASCII.GetString(cozumDizi); // Byte[]'i string'e çevirir
+            }
+            catch
+            {
+                return "Çözüm Hatası"; // Hatalı çözümleme durumunda
+            }
+        }
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=EMIR\SQLEXPRESS;Initial Catalog=DbSifreleme;Integrated Security=True;Encrypt=False");
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string metin=txtAd.Text;
+
+            //byte türünde dizi oluşturup her bir karakteri parçaladım.
+            byte[] adDizi=ASCIIEncoding.ASCII.GetBytes(metin);
+
+            //şifreli veriye toBase formatında her bir karakteri şifreleme işlemi
+            string adSifre=Convert.ToBase64String(adDizi);
+
+            string soyad=txtSoyad.Text;
+            byte[] soyadDizi = ASCIIEncoding.ASCII.GetBytes(soyad);
+            string soyadSifre = Convert.ToBase64String(soyadDizi);
+
+            string mail=txtMail.Text;
+            byte[] mailDizi = ASCIIEncoding.ASCII.GetBytes(mail);
+            string mailSifre = Convert.ToBase64String(mailDizi);
+
+            string sifre = txtSifre.Text;
+            byte[] sifreDizi = ASCIIEncoding.ASCII.GetBytes(sifre);
+            string sifreSifre = Convert.ToBase64String(sifreDizi);
+
+            string hesapNo = txtHesapNoo.Text;
+            byte[] hesapDizi = ASCIIEncoding.ASCII.GetBytes(hesapNo);
+            string hesapSifre = Convert.ToBase64String(hesapDizi);
+
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("INSERT INTO Tbl_Veriler (AD,SOYAD,MAIL,SIFRE,HESAPNO)" +
+                " VALUES (@p1,@p2,@p3,@p4,@p5)",baglanti);
+            komut.Parameters.AddWithValue("@p1",adSifre);
+            komut.Parameters.AddWithValue("@p2", soyadSifre);
+            komut.Parameters.AddWithValue("@p3", mailSifre);
+            komut.Parameters.AddWithValue("@p4", sifreSifre);
+            komut.Parameters.AddWithValue("@p5", hesapSifre);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Veriler eklendi!");
+
+            baglanti.Close();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string adCozum = txtAd.Text;
+            byte[] adCozumDizi=Convert.FromBase64String(adCozum);
+            string adVerisi=ASCIIEncoding.ASCII.GetString(adCozumDizi);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+    }
+}
